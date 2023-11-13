9f8050d6906153a95e52485864adc9deba920f7e
Shardul Kulkarni <shardulkulkarni@Sharduls-MacBook-Pro-2.local>
2023-11-13 21:48:04 +0100

    Sorted using number instead of length of words

--- feature142/main.py
+++ feature142/main.py
@@ -1,43 +1,28 @@
+def process_input(input_file):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
 
-with open('input.txt', 'r') as file:
-    lines = file.readlines()
+    # Filter out lines containing 'u'
+    lines = [line for line in lines if 'u' not in line]
 
-# Filter out lines containing 'u'
-lines = [line for line in lines if 'u' not in line]
+    # Sort lines by the number of words
+    # lines.sort(key=lambda x: len(x.split()))
+    lines.sort()
 
-# Sort lines by the number of words
-lines.sort(key=lambda x: len([word for word in x.split() if not word.isdigit()]))
+    # Print the output to stdout and save it to output.txt
+    with open("output.txt", 'w') as output_file:
+        for line in lines:
+            # Split the line into columns
+            columns = line.strip().split('\t')
 
-# Print the output
-for line in lines:
-    # Split the line into columns
-    columns = line.strip().split('\t')
+            # Exclude column 7 (index 6)
+            del columns[6]
 
-    # Exclude column 7 (index 6)
-    del columns[6]
+            # Print [number of words]:[line without column 7]
+            output_line = "{count}:{content}".format(count=len(line.split()), content='\t'.join(columns))
+            print(output_line)
+            output_file.write(output_line + '\n')
 
-    # Print [number of words excluding numeric words]:[line without column 7]
-    word_count = len([word for word in line.split() if not word.isdigit()])
-    print("{count}:\t{content}".format(count=word_count, content='\t'.join(columns)))
-
-
-# with open('input.txt', 'r') as file:
-#     lines = file.readlines()
-
-# # Filter out lines containing 'u'
-# lines = [line for line in lines if 'u' not in line]
-
-# # Sort lines by the number of words
-# lines.sort(key=lambda x: len(x.split()))
-
-# # Print the output
-# for line in lines:
-#     # Split the line into columns
-#     columns = line.strip().split('\t')
-
-#     # Exclude column 7 (index 6)
-#     del columns[6]
-
-#     # Print [number of words]:[line without column 7]
-#     word_count = sum(len(col.split()) for col in columns)
-#     print("{count}:\t{content}".format(count=word_count, content='\t'.join(columns)))
+if __name__ == "__main__":
+    input_file = "input.txt"
+    process_input(input_file)
