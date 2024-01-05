a50a4f75727e1a7d6c28534bd3e7e42db09a401a
Shardul Kulkarni <shardulkulkarni@Sharduls-MacBook-Pro-2.local>
2024-01-05 21:27:30 +0100

    changed log_level to log_level_logfile

--- feature164/main.tf
+++ feature164/main.tf
@@ -18,20 +18,18 @@ resource "null_resource" "salt" {
       "./bootstrap-salt.sh -P -M -N stable 3005",
       "service salt-master start",
       "echo 'auto_accept: True' >> /etc/salt/master",
-      "mkdir -p /var/log/salt/",
       "echo 'log_level_logfile: critical' >> /etc/salt/master",
       "pkill salt-master",
       "service salt-master restart",
-
       "sudo salt-key -A -y"
     ]
-  }
   connection {
     # Connect to the Salt Master
-    host     = "salt"
     type     = "ssh"
     user     = "root"
     password = "ubuntu"
+    host     = "salt"
+    }
   }
 }
 
@@ -42,19 +40,20 @@ resource "null_resource" "salt_minion_node1" {
 
   provisioner "remote-exec" {
     inline = [
-      "service salt-minion start",
       "apt-get install -y salt-minion",
+      "service salt-minion start",
       "echo 'master: salt' > /etc/salt/minion.d/master.conf",
+      "echo 'log_level_logfile: critical' > /etc/salt/minion.d/log_level.conf",
       "echo 'id: lsxvax' > /etc/salt/minion.d/id.conf",
-      "echo 'log_level: critical' > /etc/salt/minion.d/log_level.conf",
       "service salt-minion restart"
     ]
-  }
+
   connection {
-    host     = "node1.local"
     type     = "ssh"
     user     = "root"
     password = "ubuntu"
+    host     = "node1.local"
+    }
   }
 }
 
@@ -64,19 +63,19 @@ resource "null_resource" "salt_minion_node2" {
 
   provisioner "remote-exec" {
     inline = [
-      "service salt-minion start",
       "apt-get install -y salt-minion",
+      "service salt-minion start",
       "echo 'master: salt' > /etc/salt/minion.d/master.conf",
+      "echo 'log_level_logfile: error' > /etc/salt/minion.d/log_level.conf",
       "echo 'id: gcevyt' > /etc/salt/minion.d/id.conf",
-      "echo 'log_level: error' > /etc/salt/minion.d/log_level.conf",
       "service salt-minion restart"
     ]
-  }
   connection {
     host     = "node2.local"
     type     = "ssh"
     user     = "root"
     password = "ubuntu"
+    }
   }
 }
 
@@ -86,18 +85,18 @@ resource "null_resource" "salt_minion_node3" {
 
   provisioner "remote-exec" {
     inline = [
-      "service salt-minion start",
       "apt-get install -y salt-minion",
+      "service salt-minion start",
       "echo 'master: salt' > /etc/salt/minion.d/master.conf",
+      "echo 'log_level_logfile: warning' > /etc/salt/minion.d/log_level.conf",
       "echo 'id: foawji' > /etc/salt/minion.d/id.conf",
-      "echo 'log_level: warning' > /etc/salt/minion.d/log_level.conf",
       "service salt-minion restart"
     ]
-  }
   connection {
     host     = "node3.local"
     type     = "ssh"
     user     = "root"
     password = "ubuntu"
+    }
   }
 }
